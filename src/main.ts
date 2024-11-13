b994d8c4272b2045163c20a6f36343b2cc215054
Nikola Lukic <nikola400lukic@gmail.com>
2024-11-13 11:16:26 +0100

    CORS policy and port change

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Blogging app')
     .setDescription('The Blogging API description')
@@ -18,6 +24,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
